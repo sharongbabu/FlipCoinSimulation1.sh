5ba769d9d2e8c8cc4e787d088a0ee5c3ff4e5113
“Enter <merin@gmail.com>
2020-10-19 04:09:55 +0530

    second commit

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -9,3 +9,23 @@ echo "tail as Winnner"
 fi
 
 
+#! /bin/bash -x
+
+head=0
+tail=0
+for(( i=1;i<=10;i++))
+do
+if [ $(( RANDOM%2 )) -eq 1 ]
+then
+echo "head"
+head=$(( $head  + 1 ))
+else
+echo "tail"
+tail=$(( $tail  + 1 ))
+
+fi
+done
+echo "$tail times tail has won"
+echo "$head times head has won"
+
+
